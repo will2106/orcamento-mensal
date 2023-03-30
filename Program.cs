d532a2d0c86e4bdd3a6e0587cf0a4083e9807fd0
william santos <willdev0904@gmail.com>
2023-03-30 10:52:43 -0300

    calculo salario

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//entradas
+Console.WriteLine($"digite seu salario :");
+int salario= int.Parse (Console.ReadLine());
+
+Console.WriteLine($"digite seus gastos :");
+int gastosMes= int.Parse (Console.ReadLine());
+
+int orcamento= salario-gastosMes;
+
+if( salario > gastosMes)
+{
+
+Console.WriteLine($"Gastos dentro do Orcamento ! :)");
+
+
+
+}
+
+else{
+
+Console.WriteLine($"Orcamento estourado ! :(");
+
+
+}
+
+
